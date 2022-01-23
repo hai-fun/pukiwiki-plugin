d689cee83abec3d749e10bb24e8bb7714fabdc9b
hai-fun <haifun129@gmail.com>
2022-01-24 02:00:23 +0900

    PHP8

--- markdown.inc.php
+++ markdown.inc.php
@@ -8,15 +8,23 @@
  * @version    $Id: markdown.inc.php,v 1.2 2007-02-24 16:28:39Z sonots $
  * @package    plugin
  */
+ // v1.21 PHP8.0 & Markdown PHP 1.9.1対応 2021-12-11 byはいふん
+
+define("PLUGIN_MARKDOWN_USE_MDEXTRA", TRUE);
+
+use \Michelf\Markdown;
+use \Michelf\MarkdownExtra;
 
 function plugin_markdown_convert()
 {
-    if (defined('PLUGIN_DIR') && file_exists(PLUGIN_DIR . 'markdown.php')) {
-        $markdown = PLUGIN_DIR . 'markdown.php';
-    } elseif (defined('EXT_PLUGIN_DIR') && file_exists(EXT_PLUGIN_DIR . 'markdown.php')) {
-        $markdown = EXT_PLUGIN_DIR . 'markdown.php';
+	$extra = (PLUGIN_MARKDOWN_USE_MDEXTRA ? "Extra" : "");
+    if (defined('PLUGIN_DIR') && file_exists(PLUGIN_DIR . 'Michelf/Markdown' . $extra . '.inc.php')) {
+        $markdown = PLUGIN_DIR . 'Michelf/Markdown' . $extra . '.inc.php';
+        $markdown = PLUGIN_DIR . 'Michelf/Markdown' . $extra . '.inc.php';
+    } elseif (defined('EXT_PLUGIN_DIR') && file_exists(EXT_PLUGIN_DIR . 'Michelf/Markdown' . $extra . '.inc.php')) {
+        $markdown = EXT_PLUGIN_DIR . 'Michelf/Markdown' . $extra . '.inc.php';
     } else {
-        return "markdown(): markdown.php does not exist under " . PLUGIN_DIR . ' or ' . EXT_PLUGIN_DIR;
+        return "markdown(): Markdown" . $extra . ".inc.php does not exist under " . PLUGIN_DIR . 'Michelf/ or ' . EXT_PLUGIN_DIR . "Michelf/";
     }
 
     $args = func_get_args();
@@ -27,7 +35,7 @@ function plugin_markdown_convert()
         $body = htmlspecialchars($body);
     }
     require_once($markdown);
-    $body = Markdown($body);
+    $body = PLUGIN_MARKDOWN_USE_MDEXTRA ? MarkdownExtra::defaultTransform($body) : Markdown::defaultTransform($body);
 
     if ($noskin) {
         pkwk_common_headers();
