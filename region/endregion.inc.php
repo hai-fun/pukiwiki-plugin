d5b90b68e5e332a798f70f935bf15dca67200065
hai-fun <haifun129@gmail.com>
2022-10-25 00:30:27 +0900

    Original region plugins coded by sonots

---
+++ region/endregion.inc.php
@@ -0,0 +1,14 @@
+<?php
+/////////////////////////////////////////////////
+// PukiWiki - Yet another WikiWikiWeb clone.
+//
+// $Id: endregion.inc.php,v 1.1 2005/01/22 15:50:00 xxxxxx Exp $
+//
+
+function plugin_endregion_convert()
+{
+	return <<<EOD
+</td></tr></table>
+EOD;
+}
+?>
